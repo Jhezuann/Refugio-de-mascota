eb931dc8d6f572032c242a217bb5c22950c6fff2
Jhezuann <pjhezuann@gmail.com>
2022-05-12 15:03:59 -0430

    Actualizacion

--- refugio/settings.py
+++ refugio/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'adopcion',
-    'mascota',
+    'apps.adopcion',
+    'apps.mascota',
 ]
 
 MIDDLEWARE = [
