d6ee84731096479d13be08c348ec627db9cad480
Jhezuann <pjhezuann@gmail.com>
2022-05-15 16:20:29 -0430

    actualizar

--- mascota/urls.py
+++ mascota/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from mascota.views import mascota, mascotaView
+from mascota.views import *
 
 urlpatterns = [
     path('', mascota, name='mascota'),
     path('nuevo/', mascotaView, name='mascotaView'),
+    path('lista/', mascota_list, name='mascota_list'),
+    path('editar/<int:id_mascota>/', mascota_edit, name='mascota_edit'),
+    path('eliminar/<int:id_mascota>/', mascota_delete, name='mascota_delete'),
 ]
--- mascota/views.py
+++ mascota/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from mascota.forms import MascotaForm
+from mascota.models import Mascota
 
 # Create your views here.
 
@@ -16,3 +17,26 @@ def mascotaView(request):
 	else:
 		form =MascotaForm()
 	return render(request, 'mascota_form.html', {'form':form})
+
+def mascota_list(request):
+	mascota = Mascota.objects.all().order_by('id')
+	contexto = {'mascotas':mascota}
+	return render(request, 'mascota_list.html', contexto)
+
+def mascota_edit(request, id_mascota):
+	mascota=Mascota.objects.get(id=id_mascota)
+	if request.method=='GET':
+		form=MascotaForm(instance=mascota)
+	else:
+		form = MascotaForm(request.POST, instance=mascota)
+		if form.is_valid():
+			form.save()
+		return redirect('mascota_list')
+	return render(request, 'mascota_form.html', {'form':form})
+
+def mascota_delete(request, id_mascota):
+	mascota=Mascota.objects.get(id=id_mascota)
+	if request.method=='POST':
+		mascota.delete()
+		return redirect('mascota_list')
+	return render(request, 'mascota_delete.html', {'mascota':mascota})
