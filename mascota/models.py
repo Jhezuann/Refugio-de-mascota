dd8d3903d1494b708594ea8638c74f1faf34dd02
Krayrox <Floresmolinalevidario@gmail.com>
2022-05-18 11:33:34 -0400

    actualizacion

--- mascota/models.py
+++ mascota/models.py
@@ -3,17 +3,19 @@ from adopcion.models import Persona
 
 # Create your models here.
 
-class Vacuna(models.Model):
-	nombre=models.CharField(max_length=50)
 
-	def __str__(self):
-		return '{}'.format (self.nombre)
+class Vacuna(models.Model):
+    nombre = models.CharField(max_length=50)
+
+    def __str__(self):
+        return '{}'.format(self.nombre)
+
 
 class Mascota(models.Model):
-	nombre=models.CharField(max_length=50)
-	sexo=models.CharField(max_length=10)
-	edadAproximada=models.IntegerField()
-	fechaDeRescate=models.DateField()
-	persona = models.ForeignKey(Persona, null=True, blank=True, on_delete=models.CASCADE)
-	vacuna = models.ManyToManyField(Vacuna, blank=True)
-	
+    nombre = models.CharField(max_length=50)
+    sexo = models.CharField(max_length=10)
+    edadAproximada = models.IntegerField()
+    fechaDeRescate = models.DateField()
+    persona = models.ForeignKey(
+        Persona, null=True, blank=True, on_delete=models.CASCADE)
+    vacuna = models.ManyToManyField(Vacuna, blank=True)
