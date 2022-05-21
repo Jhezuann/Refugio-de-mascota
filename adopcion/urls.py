91dcb681ca037035e764b1de18057a6624555eb8
Jhezuann <pjhezuann@gmail.com>
2022-05-20 19:57:02 -0430

    actualizacion

--- adopcion/urls.py
+++ adopcion/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
 	path('', index),
 	path('solicitud/listar', SolicitudList.as_view(), name='SolicitudList'),
 	path('solicitud/nueva', SolicitudCreate.as_view(), name='SolicitudCreate'),
+	path('solicitud/editar/<pk>/', SolicitudUpdate.as_view(), name='SolicitudUpdate'),
+	path('solicitud/eliminar/<pk>/', SolicitudDelete.as_view(), name='SolicitudDelete'),
 ]
