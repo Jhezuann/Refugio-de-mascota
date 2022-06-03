d84ca6ba2e31af0e5c436c4c63e0064bf82d5d8b
Jhezuann <pjhezuann@gmail.com>
2022-06-02 22:11:58 -0430

    actualizacion

--- refugio/urls.py
+++ refugio/urls.py
@@ -23,8 +23,4 @@ urlpatterns = [
     path('adopcion/', include('adopcion.urls')),
     path('usuario/', include('usuario.urls')),
     path('', LoginView.as_view(template_name='mascota.html'), name='login'),
-    path('reset/password', PasswordResetView.as_view(template_name='password_reset_form.html', email_template_name='password_reset_email.html'), name='password_reset'),
-    path('reset/password_done', PasswordResetDoneView.as_view(template_name='password_reset_done.html'), name='password_reset_done'),
-    path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>.+)/', PasswordResetConfirmView.as_view(template_name='password_reset_confirm.html'), name='password_reset_confirm'),
-    path('reset/done', PasswordResetCompleteView.as_view(template_name='password_reset_complete.html'), name='password_reset_complete'),
 ]
